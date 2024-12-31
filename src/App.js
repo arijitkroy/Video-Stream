82fdadf7d5b07990c435543dc8b6aab9e4a233fe
Arijit Kumar Roy <83859073+arijitkroy@users.noreply.github.com>
2024-12-31 22:37:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import JoinScreen from "./components/JoinScreen";
 
 function App() {
   const [meetingId, setMeetingId] = useState(null);
-  const [name, setName] = useState("Participant");
+  //const [name, setName] = useState("Participant");
   const getMeetingAndToken = async(id) => {
     const meetingId = id == null? await createMeeting({ token: authToken }) : id;
     setMeetingId(meetingId);
@@ -16,16 +16,16 @@ function App() {
     setMeetingId(null);
   }
 
-  const getName = (e) => {
-    setName(e)
-  }
+  //const getName = (e) => {
+  //  setName(e)
+  //}
 
   return authToken && meetingId ? (
-    <MeetingProvider config={{ meetingId, micEnabled: true, webcamEnabled: true, name: {name}}} token={authToken}>
+    <MeetingProvider config={{ meetingId, micEnabled: true, webcamEnabled: true, name: "Participant"}} token={authToken}>
       <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
     </MeetingProvider>
   ) : (
-    <JoinScreen getMeetingAndToken={getMeetingAndToken} getName={getName} />
+    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
   )
 }
 
