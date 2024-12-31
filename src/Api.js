10e1673792a5f041e360e9da564e8ea4e983bdca
Arijit Kumar Roy <83859073+arijitkroy@users.noreply.github.com>
2024-12-31 22:36:41 +0530

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,4 @@
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIzMWI2YTUwOS0yYTI4LTRhYWItYWFlYi1hZWEwN2Y0MjIwYWYiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNTY1ODM4NCwiZXhwIjoxNzM1NzQ0Nzg0fQ.1S3DRG1AlOS21kUwbVevE7WKtBRlzg5HnaUmXxFQ16M";
+export const authToken = process.env.API_TOKEN;
 export const createMeeting = async({ token }) => {
     const res = await fetch('https://api.videosdk.live/v2/rooms', {
         method: 'POST',
